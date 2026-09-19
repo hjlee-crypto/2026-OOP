fb439af7373a422e1944a2840109015062bb9c1c
hjbeauty <hjbeauty68@gmail.com>
2026-09-20 00:22:23 +0900

    추가 및 수정

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,50 +1,25 @@
-import java.util.Scanner;
 
 public class Gugudan {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("start 단? ");
-        int startDan = scanner.nextInt();
-
-        System.out.print("end 단? ");
-        int endDan = scanner.nextInt();
-
-        System.out.print("가로 출력 개수? ");
-        int columnCount = scanner.nextInt();
+        int startDan = 2;
+        int endDan = 9;
+        int columnCount = 2;
 
         // ① 단 묶음 반복
-        for (int groupStart = startDan;
-             groupStart <= endDan;
-             groupStart += columnCount) {
-
-            int groupEnd = Math.min(
-                    groupStart + columnCount - 1,
-                    endDan
-            );
-
-            // 단 제목 출력
-            for (int dan = groupStart; dan <= groupEnd; dan++) {
-                System.out.printf("%-12s", dan + "단");
-            }
-            System.out.println();
-
+        for (int groupStart = startDan;  groupStart <= endDan;  groupStart += columnCount) {
             // ② 곱하는 수 반복
             for (int number = 1; number <= 9; number++) {
-
                 // ③ 현재 묶음의 각 단 반복
-                for (int dan = groupStart; dan <= groupEnd; dan++) {
-                    System.out.printf(
-                            "%d × %d = %-3d ",
-                            dan, number, dan * number
+                for (int dan = groupStart; dan <= (groupStart+1); dan++) {
+                    System.out.printf( "%d × %d = %-3d ",
+                                                 dan, number, dan * number
                     );
-                }
+                } // ③ 현재 묶음의 각 단 반복  end
                 System.out.println();
-            }
-
+            }// ② 곱하는 수 반복 end
             System.out.println();
-        }
+        }//① 단 묶음 반복 for end
 
-        scanner.close();
-    }
-}
+    }//main() end
+}//class  end
